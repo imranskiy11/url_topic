e3af3e4c03aa9ca9047c63fbea0f84ba5a4fcbf8
imranskiy11 <imranskiy11@gmail.com>
2022-05-21 21:45:59 +0300

    modified vocab struct

--- data_validator/vocab_struct.py
+++ data_validator/vocab_struct.py
@@ -1,5 +1,6 @@
 import collections
 from sklearn.neighbors import NearestCentroid
+from sklearn.cluster import AgglomerativeClustering
 from termcolor import colored
 
 class VocabStruct:
@@ -14,15 +15,19 @@ class VocabStruct:
         
         
         self.centroid_clf = NearestCentroid(metric=self.metric)
+        self.agg_cluster = AgglomerativeClustering(affinity='cosine', linkage='average')
+        
     
     def fill_embed_vocab(self, embedder):
         for class_name in self.vocab:
             self.embedded_vocab[class_name] = embedder.word_embeddings_list(
                 self.vocab[class_name]).detach().cpu().detach()
                 
-    def fill_centroid_vocab(self, centroid_clf):
+    def fill_centroid_vocab(self):
         if len(self.embedded_vocab) == 0:
             raise Exception(colored('Classes embedded vocab is empty', 'red'))
         for class_name in self.embedded_vocab:
-            self.centroid_clf.fit(self.embedded_vocab[class_name])
+            agg_preds = self.agg_cluster.fit_predict(self.embedded_vocab[class_name])
+            self.centroid_clf.fit(self.embedded_vocab[class_name], agg_preds)
             self.centroids_vocab[class_name] = self.centroid_clf.centroids_
+            
